532fddbbcffb8ad7c4590de6ccd866f0185100f8
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-04-22 16:31:59 -0300

    Feat: como usar if e porque não existe if ternário em kotlin.

--- src/main/kotlin/br/com/carvalho/section04/_0403/KotlinTypes.kt
+++ src/main/kotlin/br/com/carvalho/section04/_0403/KotlinTypes.kt
@@ -1,29 +1,27 @@
 package br.com.carvalho.section04._0403
 
 fun main() {
-    val myPhrase = "I'm a String"
-    val myPi = 3.14
-    val myAnswer = 42
+
+    val myInt = 199
+    val aInt: Int = 7
+
+    //java
+    //int lowest = (myInt < aInt)? myInt : aInt
+
+    //kotlin
+    val lowest = if (myInt < aInt) myInt else aInt
 
 
-    val aString: String
-    val aDouble: Double
-    val aInt: Int
-    val aByte: Byte
-    val myFloat = 1.9F
-    val myLong = 997798962315452L
-    val myInt = 9
-    val myShort: Short = 12
-    val myByte2: Byte = 127
+    val temp = 25
 
+    val isAirConditioneerOn = if( temp >= 26){
+        println("It is warm")
+        true
+    } else{
+        println("It isn't so warm")
+        false
+    }
 
-    val aLongNumber = 123_456_789
-    val theSameNumbe = 123456789
-
-
-
-    val anInt: Int = 7
-    val aLong: Long = anInt.toLong() // casting
-
+    println("Is the air conditioner on: $isAirConditioneerOn")
 
 }
---
+++ src/main/kotlin/br/com/carvalho/section04/_0405/Comments.kt
@@ -0,0 +1,22 @@
+package br.com.carvalho.section04._0405
+
+fun main() {
+//I am commentary
+
+    /*
+        val myPhrase = "I'm a String"
+        val myPi = 3.14
+        val myAnswer = 42
+    */
+
+
+}
+
+/*
+fun fooBar() {
+    /*
+    I am embedded comment
+     */
+}
+
+ */
