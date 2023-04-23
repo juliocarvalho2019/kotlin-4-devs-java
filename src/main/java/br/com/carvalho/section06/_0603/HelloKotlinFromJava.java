10e7425825a9d1da52ce7ec305331e31ea8d34d7
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-04-23 12:19:07 -0300

    Feat: Interoperabilidade de kotlin com java.

---
+++ src/main/java/br/com/carvalho/section06/_0603/HelloKotlinFromJava.java
@@ -0,0 +1,17 @@
+package br.com.carvalho.section06._0603;
+import br.com.carvalho.section05._0505.MaxKt;
+public class HelloKotlinFromJava {
+
+    public static void main(String[] args) {
+
+        Person rob = new Person("Robert", "Martin");
+
+        System.out.println("His name is " + rob.getFirstName());
+
+        rob.setFirstName("Uncle Bob");
+        System.out.println("His name is is now " + rob.getFirstName());
+
+        Integer maxInt = MaxKt.max(2001, 2022);
+        System.out.println("The max value is " + maxInt);
+    }
+}
